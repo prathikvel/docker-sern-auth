5a8277dbce4cc6df88ee2aa4f3152ebce2af82be
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-06-09 12:39:58 -0400

    Added Permissions to `findRole(s)` Output

--- api/src/repositories/role.repository.ts
+++ api/src/repositories/role.repository.ts
@@ -1,9 +1,35 @@
+import { ExpressionBuilder } from "kysely";
+import { jsonArrayFrom } from "kysely/helpers/mysql";
+
 import { db } from "../config/database";
-import { Role, NewRole, RoleUpdate } from "../models";
+import { Database, Permission, Role, NewRole, RoleUpdate } from "../models";
 import { pick } from "../utils/object.util";
 
-/** The columns to compare, including all role columns. */
-const columns = ["rol_id", "rol_name", "rol_created"] as const;
+/** The columns to filter, including all role columns and the `per_id` and
+ * `per_name` permission columns. */
+const columnsToFilter = [
+  "rol_id",
+  "rol_name",
+  "rol_created",
+  "per_id",
+  "per_name",
+] as const;
+
+/** The columns to select, including all role columns and the `per_id` and
+ * `per_name` permission columns. */
+const columnsToSelect = [
+  "rol_id",
+  "rol_name",
+  "rol_created",
+  (eb: ExpressionBuilder<Database, "role_permission">) => {
+    return jsonArrayFrom(
+      eb
+        .selectFrom("permission")
+        .select(["per_id", "per_name"])
+        .whereRef("per_id", "=", "rlp_per_id")
+    ).as("permissions");
+  },
+] as const;
 
 /**
  * The generic function to find a role based on a criterion.
@@ -17,41 +43,45 @@ const findRole = async <K extends keyof Role>(
   criterionValue: Role[K]
 ) => {
   const query = db
-    .selectFrom("role")
+    .selectFrom("role_permission")
+    .rightJoin("role", "rol_id", "rlp_rol_id")
     .where(criterion, "=", criterionValue as any);
 
-  return await query.selectAll().executeTakeFirst();
+  return await query.select(columnsToSelect).executeTakeFirst();
 };
 
 /**
- * Returns the role or undefined if the given `id` is invalid.
+ * Returns the role and its permissions or undefined if given `id` is invalid.
  *
  * @param id The role's `rol_id`
- * @returns The role or undefined if the given `id` is invalid
+ * @returns The role and its permissions or undefined if given `id` is invalid
  */
 export const findRoleById = (id: number) => findRole("rol_id", id);
 
 /**
- * Returns the role or undefined if the given `name` is invalid.
+ * Returns the role and its permissions or undefined if given `name` is invalid.
  *
  * @param name The role's `rol_name`
- * @returns The role or undefined if the given `name` is invalid
+ * @returns The role and its permissions or undefined if given `name` is invalid
  */
 export const findRoleByName = (name: string) => findRole("rol_name", name);
 
 /**
- * Returns an array of roles that match the given criteria. Returns all roles
- * if no criteria are provided. All the criteria will be compared via equality.
+ * Returns an array of roles, and their permissions, that match the given
+ * criteria. Returns all roles if no criteria are provided. All the criteria
+ * will be compared via equality.
  *
- * @param criteria An object of role fields to match with
- * @returns An array of roles that match the given criteria
+ * @param criteria An object of role or permission fields to match with
+ * @returns An array of roles, and their permissions, that match given criteria
  */
-export const findRoles = async (criteria: Partial<Role> = {}) => {
+export const findRoles = async (criteria: Partial<Role & Permission> = {}) => {
   const query = db
-    .selectFrom("role")
-    .where((eb) => eb.and(pick(criteria, columns)));
+    .selectFrom("role_permission")
+    .rightJoin("role", "rol_id", "rlp_rol_id")
+    .leftJoin("permission", "per_id", "rlp_per_id")
+    .where((eb) => eb.and(pick(criteria, columnsToFilter)));
 
-  return await query.selectAll().execute();
+  return await query.select(columnsToSelect).execute();
 };
 
 /**
