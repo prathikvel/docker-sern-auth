d26346889a64b1904d0d0a5a5a6b9f8e5ff3f326
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-06-13 20:48:57 -0400

    Removed Most Static `db` References
    If the developer changes the name of the database with the corresponding environment variable, they would have to change all static references in the initialization script. Now, there's only one static reference to change in the codebase: `USE \`db\`;`.

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -1,14 +1,13 @@
 -- -----------------------------------------------------
 -- Schema db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `db`;
 USE `db`;
 
 
 -- -----------------------------------------------------
--- Table `db`.`user`
+-- Table `user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `usr_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `usr_name` VARCHAR(255) NOT NULL,
   `usr_email` VARCHAR(255) NOT NULL,
@@ -20,9 +19,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db`.`role`
+-- Table `role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`role` (
+CREATE TABLE IF NOT EXISTS `role` (
   `rol_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `rol_name` VARCHAR(45) NOT NULL,
   `rol_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -32,24 +31,24 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db`.`user_role`
+-- Table `user_role`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`user_role` (
+CREATE TABLE IF NOT EXISTS `user_role` (
   `url_usr_id` INT UNSIGNED NOT NULL,
   `url_rol_id` INT UNSIGNED NOT NULL,
   `url_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`url_usr_id`, `url_rol_id`),
   INDEX `fk_url_usr_idx` (`url_usr_id` ASC) VISIBLE,
   INDEX `fk_url_rol_idx` (`url_rol_id` ASC) VISIBLE,
-  CONSTRAINT `fk_url_usr` FOREIGN KEY (`url_usr_id`) REFERENCES `db`.`user` (`usr_id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_url_rol` FOREIGN KEY (`url_rol_id`) REFERENCES `db`.`role` (`rol_id`) ON DELETE CASCADE)
+  CONSTRAINT `fk_url_usr` FOREIGN KEY (`url_usr_id`) REFERENCES `user` (`usr_id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_url_rol` FOREIGN KEY (`url_rol_id`) REFERENCES `role` (`rol_id`) ON DELETE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db`.`permission`
+-- Table `permission`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`permission` (
+CREATE TABLE IF NOT EXISTS `permission` (
   `per_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `per_name` VARCHAR(45) NOT NULL,
   `per_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -59,15 +58,15 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `db`.`role_permission`
+-- Table `role_permission`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`role_permission` (
+CREATE TABLE IF NOT EXISTS `role_permission` (
   `rlp_rol_id` INT UNSIGNED NOT NULL,
   `rlp_per_id` INT UNSIGNED NOT NULL,
   `rlp_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`rlp_rol_id`, `rlp_per_id`),
   INDEX `fk_rlp_rol_idx` (`rlp_rol_id` ASC) VISIBLE,
   INDEX `fk_rlp_per_idx` (`rlp_per_id` ASC) VISIBLE,
-  CONSTRAINT `fk_rlp_rol` FOREIGN KEY (`rlp_rol_id`) REFERENCES `db`.`role` (`rol_id`) ON DELETE CASCADE,
-  CONSTRAINT `fk_rlp_per` FOREIGN KEY (`rlp_per_id`) REFERENCES `db`.`permission` (`per_id`) ON DELETE CASCADE)
+  CONSTRAINT `fk_rlp_rol` FOREIGN KEY (`rlp_rol_id`) REFERENCES `role` (`rol_id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_rlp_per` FOREIGN KEY (`rlp_per_id`) REFERENCES `permission` (`per_id`) ON DELETE CASCADE)
 ENGINE = InnoDB;
