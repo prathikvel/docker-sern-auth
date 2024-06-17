900777722cbc46cb6b0a7286a1eb0d419e080b21
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-06-17 14:49:31 -0400

    Changed Datetime Precision

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `usr_name` VARCHAR(255) NOT NULL,
   `usr_email` VARCHAR(255) NOT NULL,
   `usr_password` CHAR(60) NOT NULL,
-  `usr_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `usr_created` DATETIME(3) NOT NULL DEFAULT NOW(3),
   PRIMARY KEY (`usr_id`),
   UNIQUE INDEX `usr_email_UNIQUE` (`usr_email` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -24,7 +24,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `role` (
   `rol_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `rol_name` VARCHAR(45) NOT NULL,
-  `rol_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rol_created` DATETIME(3) NOT NULL DEFAULT NOW(3),
   PRIMARY KEY (`rol_id`),
   UNIQUE INDEX `rol_name_UNIQUE` (`rol_name` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -36,7 +36,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `user_role` (
   `url_usr_id` INT UNSIGNED NOT NULL,
   `url_rol_id` INT UNSIGNED NOT NULL,
-  `url_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `url_created` DATETIME(3) NOT NULL DEFAULT NOW(3),
   PRIMARY KEY (`url_usr_id`, `url_rol_id`),
   INDEX `fk_url_usr_idx` (`url_usr_id` ASC) VISIBLE,
   INDEX `fk_url_rol_idx` (`url_rol_id` ASC) VISIBLE,
@@ -51,7 +51,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `permission` (
   `per_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `per_name` VARCHAR(45) NOT NULL,
-  `per_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `per_created` DATETIME(3) NOT NULL DEFAULT NOW(3),
   PRIMARY KEY (`per_id`),
   UNIQUE INDEX `per_name_UNIQUE` (`per_name` ASC) VISIBLE)
 ENGINE = InnoDB;
@@ -63,7 +63,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `role_permission` (
   `rlp_rol_id` INT UNSIGNED NOT NULL,
   `rlp_per_id` INT UNSIGNED NOT NULL,
-  `rlp_created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rlp_created` DATETIME(3) NOT NULL DEFAULT NOW(3),
   PRIMARY KEY (`rlp_rol_id`, `rlp_per_id`),
   INDEX `fk_rlp_rol_idx` (`rlp_rol_id` ASC) VISIBLE,
   INDEX `fk_rlp_per_idx` (`rlp_per_id` ASC) VISIBLE,
