e92424dc917cde9769436af6b974e244a5280918
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-06-18 21:53:02 -0400

    Configured Absolute Imports

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,5 +1,10 @@
-/** @type {import("ts-jest").JestConfigWithTsJest} */
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
+/** @type { import("ts-jest").JestConfigWithTsJest } */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
