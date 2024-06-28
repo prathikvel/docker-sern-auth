a38fba86b91d53ae96455befb750631e9410bd53
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-06-28 09:46:08 -0400

    Changed Username Field to Email

--- api/src/config/passport.ts
+++ api/src/config/passport.ts
@@ -4,7 +4,7 @@ import { Strategy as LocalStrategy } from "passport-local";
 import { verify, serializeUser, deserializeUser } from "@/features/auth";
 
 export const config = () => {
-  passport.use(new LocalStrategy(verify));
+  passport.use(new LocalStrategy({ usernameField: "email" }, verify));
   passport.serializeUser(serializeUser);
   passport.deserializeUser(deserializeUser);
 };
