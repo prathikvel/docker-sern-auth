24bdce4796592a6066fd266c7aed9296d733df3f
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-06-28 14:00:27 -0400

    Changed Credential Fields for Consistency

--- api/src/config/passport.ts
+++ api/src/config/passport.ts
@@ -4,7 +4,15 @@ import { Strategy as LocalStrategy } from "passport-local";
 import { verify, serializeUser, deserializeUser } from "@/features/auth";
 
 export const config = () => {
-  passport.use(new LocalStrategy({ usernameField: "email" }, verify));
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "usrEmail",
+        passwordField: "usrPassword",
+      },
+      verify
+    )
+  );
   passport.serializeUser(serializeUser);
   passport.deserializeUser(deserializeUser);
 };
