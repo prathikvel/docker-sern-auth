d30be1af9fc152d48264102b5c551661a24f7248
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-07-25 12:33:35 -0400

    Updated Authorization Repository

--- api/src/features/auth/auth.repository.ts
+++ api/src/features/auth/auth.repository.ts
@@ -1,20 +1,72 @@
+import { sql } from "kysely";
+
 import { db } from "@/configs/database.config";
 
 /**
  * Checks if the given user has the given permission based on the user's roles.
- * Returns `perName` if the user has role-based access and undefined otherwise.
+ * Returns true if the user has role-based access and false otherwise.
  *
  * @param usrId The user's `usrId`
  * @param perName The permission's `perName`
- * @returns A row, or undefined if the user doesn't have the given permission
+ * @param perPblId The permission's `perPblId`
+ * @returns A boolean representing the user's role-based access to the permission
  */
-export const checkRoleBasedAccess = (usrId: number, perName: string) => {
-  const query = db
-    .selectFrom("userRole")
-    .innerJoin("rolePermission", "rlpRolId", "urlRolId")
-    .innerJoin("permission", "perId", "rlpPerId")
-    .where("urlUsrId", "=", usrId)
-    .where("perName", "=", perName);
+export const checkRoleBasedAccess = async (
+  usrId: number,
+  perName: string,
+  perPblId: number | null
+) => {
+  const query = db.selectNoFrom((eb) =>
+    eb
+      .exists(
+        eb
+          .selectFrom("user")
+          .innerJoin("userRole", "urlUsrId", "usrId")
+          .innerJoin("rolePermission", "rlpRolId", "urlRolId")
+          .innerJoin("permission", "perId", "rlpPerId")
+          .where("usrId", "=", usrId)
+          .where("perName", "=", perName)
+          .where((eb) =>
+            eb.or([eb("perPblId", "is", null), eb("perPblId", "=", perPblId)])
+          )
+          .select(sql`1` as any)
+      )
+      .as("exists")
+  );
 
-  return query.select("perName").limit(1).executeTakeFirst();
+  return (await query.executeTakeFirstOrThrow()).exists;
+};
+
+/**
+ * Checks if the given user has the given permission based on user's individual
+ * permissions. Returns true if user has user-based access and false otherwise.
+ *
+ * @param usrId The user's `usrId`
+ * @param perName The permission's `perName`
+ * @param perPblId The permission's `perPblId`
+ * @returns A boolean representing the user's user-based access to the permission
+ */
+export const checkUserBasedAccess = async (
+  usrId: number,
+  perName: string,
+  perPblId: number | null
+) => {
+  const query = db.selectNoFrom((eb) =>
+    eb
+      .exists(
+        eb
+          .selectFrom("user")
+          .innerJoin("userPermission", "urpUsrId", "usrId")
+          .innerJoin("permission", "perId", "urpPerId")
+          .where("usrId", "=", usrId)
+          .where("perName", "=", perName)
+          .where((eb) =>
+            eb.or([eb("perPblId", "is", null), eb("perPblId", "=", perPblId)])
+          )
+          .select(sql`1` as any)
+      )
+      .as("exists")
+  );
+
+  return (await query.executeTakeFirstOrThrow()).exists;
 };
