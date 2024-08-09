0f5ea6e9cecfb3c6172a795756c1c1dff5dcd435
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-08-09 01:47:46 -0400

    Refactored User-Permission Validation

--- api/src/features/user-permission/user-permission.routes.ts
+++ api/src/features/user-permission/user-permission.routes.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import { checkExact, param, body } from "express-validator";
 
-import { handleEntitiesAuthorization as handleEntitiesAuth } from "../auth";
+import { USER_PERMISSION } from "@/configs/global.config";
+import { handleValidation } from "@/middlewares/validation.middleware";
+import { handlers } from "@/utils/routes.util";
+
+import { handleEntitiesAuthorization } from "../auth";
 import {
   getUserPermissionByUsrId,
   getUserPermissionByPerId,
@@ -10,28 +15,62 @@ import {
 
 export const userPermissionRouter = express.Router();
 
-// get userPermission
+// ------------------------------- GET ------------------------------
+
 userPermissionRouter.get(
   "/users/:urpUsrId",
-  handleEntitiesAuth("userPermission", "read", true),
-  getUserPermissionByUsrId
+  handlers({
+    validation: [
+      checkExact(param("urpUsrId", USER_PERMISSION.ERRORS.URP_USR_ID).isInt()),
+      handleValidation,
+    ],
+    middleware: handleEntitiesAuthorization("userPermission", "read", true),
+    controller: getUserPermissionByUsrId,
+  })
 );
+
 userPermissionRouter.get(
   "/permissions/:urpPerId",
-  handleEntitiesAuth("userPermission", "read", true),
-  getUserPermissionByPerId
+  handlers({
+    validation: [
+      checkExact(param("urpPerId", USER_PERMISSION.ERRORS.URP_PER_ID).isInt()),
+      handleValidation,
+    ],
+    middleware: handleEntitiesAuthorization("userPermission", "read", true),
+    controller: getUserPermissionByPerId,
+  })
 );
 
-// add userPermission
+// ------------------------------ POST ------------------------------
+
 userPermissionRouter.post(
   "/",
-  handleEntitiesAuth("userPermission", "create", true),
-  addUserPermission
+  handlers({
+    validation: [
+      checkExact([
+        body("urpUsrId", USER_PERMISSION.ERRORS.URP_USR_ID).isInt(),
+        body("urpPerId", USER_PERMISSION.ERRORS.URP_PER_ID).isInt(),
+      ]),
+      handleValidation,
+    ],
+    middleware: handleEntitiesAuthorization("userPermission", "create", true),
+    controller: addUserPermission,
+  })
 );
 
-// remove userPermission
+// ----------------------------- DELETE -----------------------------
+
 userPermissionRouter.delete(
   "/users/:urpUsrId/permissions/:urpPerId",
-  handleEntitiesAuth("userPermission", "delete", true),
-  removeUserPermission
+  handlers({
+    validation: [
+      checkExact([
+        param("urpUsrId", USER_PERMISSION.ERRORS.URP_USR_ID).isInt(),
+        param("urpPerId", USER_PERMISSION.ERRORS.URP_PER_ID).isInt(),
+      ]),
+      handleValidation,
+    ],
+    middleware: handleEntitiesAuthorization("userPermission", "delete", true),
+    controller: removeUserPermission,
+  })
 );
