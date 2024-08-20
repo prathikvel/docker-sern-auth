787ce2e5a517995b592cd4735418aeb2d839eaa6
pvelaga2 <13081936+prathikvel@users.noreply.github.com>
2024-08-20 16:50:52 -0400

    Reformatted Type Declaration

--- api/src/@types/express/index.d.ts
+++ api/src/@types/express/index.d.ts
@@ -3,10 +3,7 @@ import { User as AppUser } from "@/features/user";
 declare global {
   namespace Express {
     interface User extends Omit<AppUser, "usrPassword"> {
-      authorization: {
-        entity?: string[];
-        entitySet?: string[];
-      } | null;
+      authorization: { entity?: string[]; entitySet?: string[] } | null;
     }
   }
 }
